2a34f2020c12874ac1df82c70e9cea55c3c256d2
Emeraldik <Emerlidfeed@yandex.ru>
2022-10-25 20:57:40 +0300

    Add exact search
    Exact search for pepper module
    some fixes

--- modules/pepper.py
+++ modules/pepper.py
@@ -49,58 +49,82 @@ def main():
 			if x: 
 				mb.append(x['title'])
 
+		if len(mb) == 0:
+			print('Found : Nothing')
+
+		print('\nPossible Options : ')
 		for k, v in enumerate(mb, 1):
 			print(f'\t{k} | {v}')
 
 		check = int(input('\nInput number: (1 or 2 or 3) : ')) - 1
 
+		question = int(input('Exact search? : (1 = YES || or || Another number = NO) : '))
+
 		search = mb[check]
+		search = search.replace(' ', '%20')
+		url = f'https://pepper.ru/search?q={search}'
 
-		for i in finder:
-			x = i.find('a', attrs={'class':'cept-tt thread-link linkPlain thread-title--list js-thread-title'}).text
-			#print(x)
-			
-			normalized1 = x.lower()
-			normalized2 = search.lower()
-			matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
-			#print(matcher.real_quick_ratio(), matcher.quick_ratio(), matcher.ratio())
-			if matcher.ratio() < 0.35:
-				continue
+		if question == 1:
+			response = s.get(url)
 
-			
-			prices = []
-			for j in adds:
-				prices.append(i.find('span', attrs={'class':j}))
+		if response:
+			if question == 1:
+				soup = BS(response.content, 'lxml')
+				finder = soup.find_all('article', id = re.compile('^thread'))
 
-			prices = [j for j in prices if j != None]
-			if len(prices) == 0:
-				continue
-			
-			price = prices[0]
+			counter = 0
+			for i in finder:
+				x = i.find('a', attrs={'class':'cept-tt thread-link linkPlain thread-title--list js-thread-title'}).text
+				#print(x)
+				
+				normalized1 = x.lower()
+				normalized2 = search.lower()
+				matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
+				#print(matcher.real_quick_ratio(), matcher.quick_ratio(), matcher.ratio())
+				if matcher.ratio() < 0.35:
+					continue
+
+				
+				prices = []
+				for j in adds:
+					prices.append(i.find('span', attrs={'class':j}))
+
+				prices = [j for j in prices if j != None]
+				if len(prices) == 0:
+					continue
+				
+				price = prices[0]
+				#print(prices)
 
 
-			discount = i.find('span', attrs={'class':'space--ml-1 size--all-l size--fromW3-xl'})
-			#print(discount)
-			#print(price)
-			ended = i.find('span', attrs={'class':'size--all-s text--color-grey space--l-1 space--r-2 cept-show-expired-threads hide--toW3'})
-			if ended:
-				continue
-
-			price = price.text
-			price = price.strip('₽')
-			price = price.replace(' ', '')
-					
-			#print(discount)
-			if discount != None:
-				discount = discount.text
-				disc = discount.strip('%()-')
+				discount = i.find('span', attrs={'class':'space--ml-1 size--all-l size--fromW3-xl'})
 				#print(discount)
+				#print(price)
+				ended = i.find('span', attrs={'class':'size--all-s text--color-grey space--l-1 space--r-2 cept-show-expired-threads hide--toW3'})
+				if ended:
+					continue
+
+				counter += 1
+
+				price = price.text
+				price = price.strip('₽')
+				price = price.replace(' ', '')
+						
+				#print(discount)
+				if discount != None:
+					discount = discount.text
+					disc = discount.strip('%()-')
+					#print(discount)
 
 
-				prc = str(round(int(price) * (100 - int(disc))*0.01))
-				print(f'\n{x}\nPrice : {price} (Discount : {discount}) || Total : {prc}\n' + '-'*80)
+					prc = str(round(int(price) * (100 - int(disc))*0.01))
+					print(f'\n{x}\nPrice : {price} (Discount : {discount}) || Total : {prc}\n' + '-'*80)
+				else:
+					print(f'\n{x}\nPrice : {price}\n' + '-'*80)
+
+			if counter != 0:
+				print(f'Found : {counter} options')
 			else:
-				print(f'\n{x}\nPrice : {price}\n' + '-'*80)
-			
+				print('Found : Nothing')
 if __name__ == '__main__':
 	main()
